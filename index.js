4e3e51f184ee73d3291f28b1603fa300a563da5a
lapshaffer <lapshaffer@gmail.com>
2021-08-10 16:53:40 -0400

    fleshed out inquire.js with questions and logic

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
     // initialize employee files? Or maybe do that in the inquirer file?
 
 const fs = require('fs');
+const inquireInit = require("./src/inquire");
--- src/inquire.js
+++ src/inquire.js
@@ -8,9 +8,145 @@
 
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
-        {
-            
-        }
-    ])
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+
+const teamMembers = [];
+
+const managerQuestions = [
+    {
+        type: 'input',
+        message: 'Hello, team manager! Please enter your name.',
+        name: 'managerName'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your ID.',
+        name: 'managerId'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address.',
+        name: 'managerEmail'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your office number',
+        name: 'managerOfficeNumber'
+    },
+    {
+        type: 'list',
+        message: 'Which team member type would you like to add next?',
+        choices: ['Engineer', 'Intern', 'None'],
+        name: 'nextMember'
+    }
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "Please enter the name of this engineer",
+        name: 'engineerName'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the ID for this engineer',
+        name: 'engineerId'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the email address for this engineer',
+        name: 'engineerEmail'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the GitHub username for this engineer',
+        name: 'engineerGithub'
+    },
+    {
+        type: 'list',
+        message: 'Which team member type would you like to add next?',
+        choices: ['Engineer', 'Intern', 'None'],
+        name: 'nextMember' 
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: 'Please enter the name of this intern',
+        name: 'internName'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the ID for this intern',
+        name: 'internId'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the email address for this intern',
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the school this intern attends',
+        name: 'internSchool'
+    },
+    {
+        type: 'list',
+        message: 'Which team member type would you like to add next?',
+        choices: ['Engineer', 'Intern', 'None'],
+        name: 'nextMember'
+    }
+];
+
+function managerInquire() {
+    inquirer
+        .prompt(managerQuestions)
+        .then(responses => {
+            const manager = new Manager(responses.managerName, responses.managerId, responses.managerEmail, responses.managerOfficeNumber);
+            teamMembers.push(manager);
+            if (responses.nextMember === 'Engineer') {
+                engineerInquire();
+            } else if (responses.nextMember === 'Intern') {
+                internInquire();
+            } else {
+
+            }
+        })
+    };
+
+function engineerInquire() {
+    inquirer
+        .prompt(engineerQuestions)
+        .then(responses => {
+            const engineer = new Engineer(responses.engineerName, responses.engineerId, responses.engineerEmail, responses.engineerGithub);
+            teamMembers.push(engineer);
+            if (responses.nextMember === 'Engineer') {
+                engineerInquire();
+            } else if (responses.nextMember === 'Intern') {
+                internInquire();
+            } else {
+
+            }
+        })
+    };
+
+function internInquire() {
+    inquirer
+        .prompt(internQuestions)
+        .then(responses => {
+            const intern = new Intern(responses.internName, responses.internId, responses.internEmail, responses.internSchool);
+            teamMembers.push(intern);
+            if (responses.nextMember === 'Engineer') {
+                engineerInquire();
+            } else if (responses.nextMember === 'Intern') {
+                internInquire();
+            } else {
+
+            }
+        })
+    };
+
+managerInquire()
